7ee9a4e41e1cd10234dd2d1f93d59905c4f275d8
yhan <hanyi8110@gmail.com>
2022-02-20 16:24:53 +0100

    Prepare EF CORE vitualized view

--- BlazorApp.Playground/Data/Flight.cs
+++ BlazorApp.Playground/Data/Flight.cs
@@ -11,3 +11,31 @@ public class Flight
     public int FlightId { get; }
     public string Summary { get; set; }
 }
+
+public class FlightService
+{
+    public static Flight[] allFlights = Enumerable.Range(1, 10_000).Select(FlightBuilder.Build).ToArray();
+
+
+    public static async Task<IEnumerable<Flight>> GetChunk(int startIdx, int chunkSize, CancellationToken ct)
+    {
+        return new ArraySegment<Flight>(allFlights, startIdx, chunkSize);
+    }
+}
+
+public class FlightBuilder
+{
+    private static Random random = new Random();
+
+    public static Flight Build(int id)
+    {
+        return new Flight(id, RandomString(10));
+    }
+
+    public static string RandomString(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+}
